c169d416ec2a6f4d73a40b8fb5080efc78c2824c
Tonyle2001 <Tonyle200185@yahoo.com>
2022-05-20 22:39:47 -0500

    layout beginning

--- stop_watch.py
+++ stop_watch.py
@@ -1,1 +1,24 @@
 import PySimpleGUI as sg
+
+sg.theme('black')
+layout = [
+    [sg.VPush()],
+    [sg.Text('Time')],
+    [sg.Button('Start'), sg.Button('Lap')],
+    [sg.VPush()]
+]
+
+window = sg.Window('Stopwatch',
+                   layout,
+                   size = (300,300),
+                   element_justification= 'center'
+                   )
+
+while True:
+
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
